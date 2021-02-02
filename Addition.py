4526f203b43e5dbfd10ee645a3a96596fd066b33
dhillon1 <simrandhillon5486@gmail.com>
2021-02-02 12:35:57 -0500

    Version 1 - Addition

---
+++ Addition.py
@@ -0,0 +1,6 @@
+def addition(z,y):
+    return z+y
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+c = addition(a,b)
+print("Sum of {} and {} = {} ".format(a,b,c))
