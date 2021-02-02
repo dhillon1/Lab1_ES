b1ff2a765ca33e2c2b409ee1b3d9239412d60e79
dhillon1 <simrandhillon5486@gmail.com>
2021-02-02 13:40:30 -0500

    Version - Multiplication.py

---
+++ Multiplication.py
@@ -0,0 +1,6 @@
+def multiplication(z,y):
+    return z*y
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+c = multiplication(a,b)
+print("Multiplication of {} and {} = {} ".format(a,b,c))
